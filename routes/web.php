d07355deac6d48f0fae9b686fe6e2347484cda36
Dominik Nowak <domin1204@gmail.com>
2016-11-02 11:14:47 +0100

    Zmienione routes do nowego stylu laravel

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,22 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
 |
 */
 
-// user not logged in
-Route::group(['middleware' => 'web'], function(){
-    Auth::routes();
+Auth::routes();
 
-    Route::get('/', function () {
-      return redirect('/login');
-    });
-
+Route::get('/', function () {
+  return redirect('/login');
 });
 
-// App / user logged in
-Route::group(['middleware' => ['web', 'auth']], function(){
+Route::group(['middleware' => 'auth'], function(){
   Route::get('/', function(){
     return view('dashboard');
   });
