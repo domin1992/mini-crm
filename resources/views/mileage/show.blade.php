f44c2b0c180f99ac945f8ccb0136a3c4dc475a5b
Dominik Nowak <domin1204@gmail.com>
2017-05-12 08:04:40 +0200

    Added number formatting

--- resources/views/mileage/show.blade.php
+++ resources/views/mileage/show.blade.php
@@ -63,9 +63,9 @@
                                 <td>{{ $record->departure }}</td>
                                 <td>{{ $record->route_description }}</td>
                                 <td>{{ $record->reason }}</td>
-                                <td>{{ $record->distance }}</td>
-                                <td>{{ $record->rate }}</td>
-                                <td>{{ $record->value }}</td>
+                                <td>{{ number_format($record->distance, 2, ',', ' ') }}</td>
+                                <td>{{ number_format($record->rate, 4, ',', ' ') }}</td>
+                                <td>{{ number_format($record->value, 2, ',', ' ') }}</td>
                                 <td>{{ $record->comments }}</td>
                             </tr>
                         @empty
