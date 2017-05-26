ffdece4dfc2845bf5243371ddcae2bab2d4243ce
Dominik Nowak <domin1204@gmail.com>
2017-05-26 18:40:48 +0200

    Added mail message on invoice created

---
+++ app/Mail/InvoiceCreated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class InvoiceCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $invoice;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($invoice)
+    {
+        $this->invoice = $invoice;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->title('Faktura '.$this->invoice->invoice_number)
+                    ->view('email.invoice-created')
+                    ->text('email.invoice-created-plain');
+    }
+}
---
+++ resources/views/email/invoice.created-plain.blade.php
@@ -0,0 +1,5 @@
+Zencore Creative Agency
+
+Faktura {{ $invoice->invoice_number }}
+
+Dziękujemy za złożenie zamówienia, w załączniku przesyłamy fakturę w formacie PDF
