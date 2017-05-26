78e3dc54164ba726a4d1f8c2af8581cd49eb4585
Dominik Nowak <domin1204@gmail.com>
2017-05-26 12:35:37 +0200

    Added api creating invoice

---
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Invoice;
+use App\InvoicePosition;
+use App\Owner;
+use Carbon\Carbon;
+
+class InvoiceController extends Controller
+{
+    public function store(Request $request){
+        //
+    }
+}
