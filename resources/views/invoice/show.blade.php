c599ca71f4bf160b36585bc77aaab2388ec2f0f9
Dominik Nowak <domin1204@gmail.com>
2017-05-26 12:55:53 +0200

    Changed formatting

--- resources/views/invoice/show.blade.php
+++ resources/views/invoice/show.blade.php
@@ -1,159 +1,158 @@
 @extends('layouts.master')
 
 @section('content')
-<section class="content-header">
-  <h1>
-    Faktura
-  </h1>
-</section>
-<section class="invoice content">
-  <div class="row">
-    <div class="col-xs-2">
-      <h2>
-        <img src="/img/logo.png" class="img-responsive" alt="">
-      </h2>
-    </div>
-  </div>
-  <div class="row invoice-info">
-    <div class="col-sm-6 invoice-col">
-      <b>
-        Faktura&nbsp;
-        @if($invoice->advance)
-        zaliczkowa&nbsp;
-        @endif
-        {{ $invoice->invoice_number }}
-      </b>
-    </div>
-    <div class="col-sm-6 invoice-col">
-      {{ $invoice->issue_date }}, {{ $invoice->issue_city }}
-    </div>
-  </div>
-  <div class="row invoice-info">
-    <div class="col-sm-6 invoice-col">
-      <strong>Sprzedawca</strong>
-      <address>
-        {{ $owner->name }}<br>
-        NIP: {{ $owner->vat_number }}<br>
-        ul. {{ $owner->street }}<br>
-        {{ $owner->postcode }} {{ $owner->city }}<br>
-        Tel: {{ $owner->phone }}<br>
-        Email: {{ $owner->email }}
-      </address>
-    </div>
-    <div class="col-sm-6 invoice-col">
-      <strong>Nabywca</strong>
-        <address>
-          @foreach($invoice->client()->get() as $client)
-            {{ $client->company }}<br>
-            NIP: {{ $client->nip }}<br>
-          @endforeach
-          @foreach($invoice->address()->get() as $address)
-            {{ $address->street }} {{ $address->street_number }}
-            @if($address->apartment_number)
-              m. {{ $address->apartment_number }}
+    <section class="content-header">
+        <h1>
+            Faktura
+        </h1>
+    </section>
+    <section class="invoice content">
+        <div class="row">
+            <div class="col-xs-2">
+                <h2>
+                    <img src="/img/logo.png" class="img-responsive" alt="">
+                </h2>
+            </div>
+        </div>
+        <div class="row invoice-info">
+            <div class="col-sm-6 invoice-col">
+                <b>
+                Faktura&nbsp;
+                @if($invoice->advance)
+                    zaliczkowa&nbsp;
+                @endif
+                {{ $invoice->invoice_number }}
+                </b>
+            </div>
+            <div class="col-sm-6 invoice-col">
+                {{ $invoice->issue_date }}, {{ $invoice->issue_city }}
+            </div>
+        </div>
+        <div class="row invoice-info">
+            <div class="col-sm-6 invoice-col">
+                <strong>Sprzedawca</strong>
+                <address>
+                    {{ $owner->name }}<br>
+                    NIP: {{ $owner->vat_number }}<br>
+                    ul. {{ $owner->street }}<br>
+                    {{ $owner->postcode }} {{ $owner->city }}<br>
+                    Tel: {{ $owner->phone }}<br>
+                    Email: {{ $owner->email }}
+                </address>
+            </div>
+            <div class="col-sm-6 invoice-col">
+                <strong>Nabywca</strong>
+                <address>
+                    @foreach($invoice->client()->get() as $client)
+                        {{ $client->company }}<br>
+                        NIP: {{ $client->nip }}<br>
+                    @endforeach
+                    @foreach($invoice->address()->get() as $address)
+                        {{ $address->street }} {{ $address->street_number }}
+                        @if($address->apartment_number)
+                            m. {{ $address->apartment_number }}
+                        @endif
+                        <br>
+                        {{ $address->postcode }} {{ $address->city }}<br>
+                    @endforeach
+                </address>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>L.p.</th>
+                            <th>Nazwa towaru/usługi</th>
+                            <th>Ilość</th>
+                            <th>JM</th>
+                            <th>Cena netto</th>
+                            <th>Wartość netto</th>
+                            <th>Stawka VAT</th>
+                            <th>Kwota VAT</th>
+                            <th>Wartość z&nbsp;VAT</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($invoice->invoicePositions()->get() as $key => $position)
+                            <tr>
+                                <td>{{ $key + 1 }}</td>
+                                <td>{{ $position->name }}</td>
+                                <td>{{ $position->quantity }}</td>
+                                <td>{{ $position->measure_unit }}</td>
+                                <td>{{ number_format($position->price_tax_excl, 2, ',', '') }} zł</td>
+                                <td>{{ number_format($position->price_tax_excl * $position->quantity, 2, ',', '') }} zł</td>
+                                @foreach($position->tax()->get() as $tax)
+                                    <td>{{ $tax->display }}</td>
+                                    <td>{{ number_format($tax->value * ($position->price_tax_excl * $position->quantity), 2, ',', '') }} zł</td>
+                                    <td>{{ number_format($tax->value * ($position->price_tax_excl * $position->quantity) + ($position->price_tax_excl * $position->quantity), 2, ',', '') }} zł</td>
+                                @endforeach
+                            </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="4"></td>
+                            <td><strong>Razem</strong></td>
+                            <td>{{ number_format($invoice->sumPositionsValueTaxExcl, 2, ',', '') }} zł</td>
+                            <td>-</td>
+                            <td>{{ number_format($invoice->sumPositionsTaxValue, 2, ',', '') }} zł</td>
+                            <td>{{ number_format($invoice->sumPositionsValueTaxIncl, 2, ',', '') }} zł</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6">
+                <p class="lead">Do zapłaty:</p>
+                <p class="lead-bottom">{{ number_format($invoice->sumPositionsValueTaxIncl, 2, ',', '') }} zł</p>
+                <p class="lead">Termin płatności:</p>
+                <p class="lead-bottom">{{ $invoice->payment_date }}</p>
+                @if($invoice->payment_method == 0)
+                    <p class="lead">Płatność przelewem:</p>
+                    <p class="lead-bottom">
+                        {{ $owner->name }}<br />
+                        {{ $owner->bank_account_number }}<br />
+                        ({{ $owner->bank_name }})<br />
+                        W tytule przelewu prosimy wpisać numer faktury <strong>{{ $invoice->invoice_number }}</strong>
+                    </p>
+                @elseif($invoice->payment_method == 1)
+                    <p class="lead">Płatność gotówką</p>
+                @elseif($invoice->payment_method == 2)
+                    <p class="lead">Płatność gotówką (zapłacono)</p>
+                @elseif($invoice->payment_method == 3)
+                    <p class="lead">Przelew (zapłacono)</p>
+                @elseif($invoice->payment_method == 4)
+                    <p class="lead">PayU</p>
+                @elseif($invoice->payment_method == 5)
+                    <p class="lead">PayU (zapłacono)</p>
+                @endif
+            </div>
+            @if($invoice->comment)
+                <div class="col-xs-6">
+                    <p class="lead">Uwagi</p>
+                    <p>{{ $invoice->comment }}</p>
+                </div>
             @endif
-          <br>
-          {{ $address->postcode }} {{ $address->city }}<br>
-          @endforeach
-        </address>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-xs-12 table-responsive">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>L.p.</th>
-            <th>Nazwa towaru/usługi</th>
-            <th>Ilość</th>
-            <th>JM</th>
-            <th>Cena netto</th>
-            <th>Wartość netto</th>
-            <th>Stawka VAT</th>
-            <th>Kwota VAT</th>
-            <th>Wartość z&nbsp;VAT</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($invoice->invoicePositions()->get() as $key => $position)
-            <tr>
-              <td>{{ $key + 1 }}</td>
-              <td>{{ $position->name }}</td>
-              <td>{{ $position->quantity }}</td>
-              <td>{{ $position->measure_unit }}</td>
-              <td>{{ number_format($position->price_tax_excl, 2, ',', '') }} zł</td>
-              <td>{{ number_format($position->price_tax_excl * $position->quantity, 2, ',', '') }} zł</td>
-              @foreach($position->tax()->get() as $tax)
-                <td>{{ $tax->display }}</td>
-                <td>{{ number_format($tax->value * ($position->price_tax_excl * $position->quantity), 2, ',', '') }} zł</td>
-                <td>{{ number_format($tax->value * ($position->price_tax_excl * $position->quantity) + ($position->price_tax_excl * $position->quantity), 2, ',', '') }} zł</td>
-              @endforeach
-            </tr>
-          @endforeach
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="4"></td>
-            <td><strong>Razem</strong></td>
-            <td>{{ number_format($invoice->sumPositionsValueTaxExcl, 2, ',', '') }} zł</td>
-            <td>-</td>
-            <td>{{ number_format($invoice->sumPositionsTaxValue, 2, ',', '') }} zł</td>
-            <td>{{ number_format($invoice->sumPositionsValueTaxIncl, 2, ',', '') }} zł</td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col-xs-6">
-      <p class="lead">Do zapłaty:</p>
-      <p class="lead-bottom">{{ number_format($invoice->sumPositionsValueTaxIncl, 2, ',', '') }} zł</p>
-      <p class="lead">Termin płatności:</p>
-      <p class="lead-bottom">{{ $invoice->payment_date }}</p>
-      @if($invoice->payment_method == 0)
-        <p class="lead">Płatność przelewem:</p>
-        <p class="lead-bottom">
-          {{ $owner->name }}<br />
-          {{ $owner->bank_account_number }}<br />
-          ({{ $owner->bank_name }})<br />
-          W tytule przelewu prosimy wpisać numer faktury <strong>{{ $invoice->invoice_number }}</strong>
-        </p>
-      @elseif($invoice->payment_method == 1)
-        <p class="lead">Płatność gotówką</p>
-      @elseif($invoice->payment_method == 2)
-        <p class="lead">Płatność gotówką (zapłacono)</p>
-      @elseif($invoice->payment_method == 3)
-        <p class="lead">Przelew (zapłacono)</p>
-      @elseif($invoice->payment_method == 4)
-        <p class="lead">PayU</p>
-      @elseif($invoice->payment_method == 5)
-        <p class="lead">PayU (zapłacono)</p>
-      @endif
-    </div>
-    @if($invoice->comment)
-      <div class="col-xs-6">
-        <p class="lead">Uwagi</p>
-        <p>{{ $invoice->comment }}</p>
-      </div>
-    @endif
-  </div>
-  <div class="row no-print">
-    <div class="col-xs-12">
-      <a href="/invoice-print/{{ $invoice->id }}" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
-      {{-- <button class="btn btn-primary pull-right" style="margin-right: 5px;"><i class="fa fa-download"></i> Generate PDF</button> --}}
-    </div>
-  </div>
-</section>
-<div class="clearfix"></div>
-@include('employee.partials.modal-employee-delete')
+        </div>
+        <div class="row no-print">
+            <div class="col-xs-12">
+                <a href="/invoice-print/{{ $invoice->id }}" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
+                {{-- <button class="btn btn-primary pull-right" style="margin-right: 5px;"><i class="fa fa-download"></i> Generate PDF</button> --}}
+            </div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+    @include('employee.partials.modal-employee-delete')
 @endsection
 
 @section('script')
-<script>
-  $('.delete-employee').click(function(){
-    $('#form-employee-delete').attr('action', '/employee/' + $(this).data('employee'));
-    $('.modal-employee-delete').modal('show');
-  });
-</script>
+    <script>
+        $('.delete-employee').click(function(){
+            $('#form-employee-delete').attr('action', '/employee/' + $(this).data('employee'));
+            $('.modal-employee-delete').modal('show');
+        });
+    </script>
 @endsection
