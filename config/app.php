5b8252a783b26315f543133b26c6e0b5af3ee0e5
Unknown <domin1204@gmail.com>
2018-05-24 12:39:18 +0200

    Added nessesary packages

--- config/app.php
+++ config/app.php
@@ -158,6 +158,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Packages Service Providers...
+         */
+        Laravel\Tinker\TinkerServiceProvider::class,
+
     ],
 
     /*
